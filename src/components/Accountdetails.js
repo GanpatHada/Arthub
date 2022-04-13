4fcbf477e6a8f6b5421817ba52ae14c6bace908d
GanpatHada <hadaganpat42@gmail.com>
2022-04-13 18:26:26 +0530

    commit 13/04/22

---
+++ src/components/Accountdetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Accountdetails = () => {
+  return (
+    <div>Here is my Account details</div>
+  )
+}
+
+export default Accountdetails
---
+++ src/components/seller/Sellinghistory.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sellinghistory = () => {
+  return (
+    <div>Sellinghistory</div>
+  )
+}
+
+export default Sellinghistory
